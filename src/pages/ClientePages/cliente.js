70e56fdd740e8372ce66dbd9f7eac0f8430eec2a
Isabella Pacheco <isabellapacheco404@gmail.com>
2023-11-15 12:25:03 -0300

    Menu do cliente feito

--- src/pages/ClientePages/cliente.js
+++ src/pages/ClientePages/cliente.js
@@ -1,10 +1,11 @@
 import Style from './CSS/PerfilPage.module.css'
+import NavbarComponent from '../../componentes/imgs/ClientComponents/Navbar/NavBar';
 
 function Cliente() {
   return (
       <div>
         <div className={Style.backgroundContainer}>
-          
+          <NavbarComponent/>
 
         </div>
       </div>
