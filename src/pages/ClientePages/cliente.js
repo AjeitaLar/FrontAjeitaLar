5e6480f48ee61e3e2a680b2cfb36c3d21efd61e6
Isabella Pacheco <isabellapacheco404@gmail.com>
2023-11-15 14:06:27 -0300

    estruturação do primeiro componente do cliente

--- src/pages/ClientePages/cliente.js
+++ src/pages/ClientePages/cliente.js
@@ -1,14 +1,19 @@
 import Style from './CSS/PerfilPage.module.css'
 import NavbarComponent from '../../componentes/imgs/ClientComponents/Navbar/NavBar';
+import PerfilComponent from '../../componentes/imgs/ClientComponents/PerfilComp/PerfilComponet';
 
 function Cliente() {
   return (
-      <div>
+    <div>
+      <div className={Style.body}>
+
         <div className={Style.backgroundContainer}>
-          <NavbarComponent/>
+          <NavbarComponent />
+          <PerfilComponent />
 
         </div>
       </div>
+    </div>
   );
 }
 
