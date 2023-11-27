8eeeab260fae60c1f56cc01ffc07f97b9caa6d6b
Ítalo Araújo Silva Acioly <italoaraujo153351@gmail.com>
2023-11-27 13:15:14 -0300

    Artigos 80% concluido

--- src/componentes/Prestadorcomponents/Artigos.js
+++ src/componentes/Prestadorcomponents/Artigos.js
@@ -1,16 +1,23 @@
 import './css/Artigos.css'
 import NavBar from './NavBar'
 import CircEco from '../imgs/Group 12.png'
-import Curso from '../imgs/Cursos.png'
+import Curso from '../imgs/Cursos1.png'
+import Mala from '../imgs/Maleta.png'
+import Userchat from '../imgs/Userchat.png'
+import Jornal from '../imgs/Jornal.png'
+import UserFlecha from '../imgs/UserFlecha.png'
+import Not1 from '../imgs/not1.jpg'
+import Not2 from '../imgs/not2.jpg'
+import Not3 from '../imgs/not3.jpg'
 
-function Artigos(){
-    return(
+function Artigos() {
+    return (
 
         <section className='Artigos'>
 
-        <NavBar />
+            <NavBar />
 
-        <section className='Curso'>
+            <section className='Curso'>
 
                 <img src={Curso} alt='Imagem Curso' className='ImgCurso' />
                 <div className='TextCurso'>
@@ -19,13 +26,56 @@ function Artigos(){
                 </div>
             </section>
 
+            <section className='tags'>
 
+                <div className='Mei'>
+                    <img src={UserFlecha} alt='User com Flecha' className='UserFlecha' />
+                    <h1>Por que MEI?</h1>
+                </div>
 
+                <div className='Cnpj'>
+                    <img src={Jornal} alt='Jornal' className='Jornal' />
+                    <h1>CNPJ, meu amigo ou inimigo?</h1>
+                </div>
 
+                <div className='Como'>
+                    <img src={Userchat} alt='User com Chat' className='Userchat' />
+                    <h1>Como se Profissionalizar</h1>
+                </div>
 
+                <div className='ArteNegocios'>
+                    <img src={Mala} alt='Mala' className='Mala' />
+                    <h1>Arte dos negocios</h1>
+                </div>
 
+            </section>
 
-        <section className='Eco1'>
+            <div className='NoticiaText'>
+            <h1>Noticias do trabalhador</h1>
+            </div>
+
+            <section className='Noticias'>
+
+                <div className='Not1'>
+                    <img src={Not1} alt='Noticia 1' className='Noticia1' />
+                    <h2>AUTÔNOMOS</h2>
+                    <h1>Desemprego é o que mais motiva trabalho autônomo no Brasil, mostra pesquisa</h1>
+                </div>
+
+                <div className='Not2'>
+                    <img src={Not2} alt='Noticia 2' className='Noticia2' />
+                    <h2>AUTÔNOMOS E CLIENTES</h2>
+                    <h1>Relação cliente e prestador: entenda a sua importância para o seu negócio!</h1>
+                </div>
+
+                <div className='Not3'>
+                    <img src={Not3} alt='Noticia 3' className='Noticia3' />
+                    <h2>AUTÔNOMOS</h2>
+                    <h1>Trabalhador Autônomo – é reconhecido como relação trabalhista? Impactos e direitos</h1>
+                </div>
+            </section>
+
+            <section className='Eco1'>
 
                 <img src={CircEco} alt='Imagem ecologica' className='ImgCircEco1' />
                 <div className='TextEco1'>
