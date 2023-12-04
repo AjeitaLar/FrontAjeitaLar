bbb4326fa63cc16ba3825d043494b6f58b95f70f
Ítalo Araújo Silva Acioly <italoaraujo153351@gmail.com>
2023-12-04 12:06:44 -0300

    cadastro e login

--- src/componentes/Registro_Login/Registro/Registro.js
+++ src/componentes/Registro_Login/Registro/Registro.js
@@ -1,29 +1,38 @@
 import './Registro.css'
 import { Link } from "react-router-dom";
+import LinhasLogin from '../../imgs/REGISTRE-SE (2).png'
+import Logo from '../../imgs/logo doido.png'
 
 function RegistroCliente() {
     return (
-        <div className="auth-containerr">
-            <h2>Registre-se</h2>
-            <form>
-                <div>
-                    <input className='Usuario' type="text" placeholder="Digite seu nome" />
-                    <input className='Usuario' type="text" placeholder="Digite seu sobrenome" />
-                </div>
-                <div>
-                    <input className='Email' type="email" placeholder="Digite seu email" />
-                </div>
-                <div>
-                    <input className='Senha' type="password" placeholder="Digite sua senha" />
-                </div>
+        <section className='Registro_fundo2'>
 
-                <div>
-                <input type="checkbox" id="scales" name="scales" />
-                <label for="scales">Aceito os termos de privacidade</label>
-                </div>
-                <Link to='/LoginCliente'><button className='Btn_Registro' type="submit">Registre-se</button></Link>
-            </form>
-        </div>
+            <img src={LinhasLogin} className='Linhas_Registro2' alt="Linhas Laranja" />
+            <img src={Logo} className='Logo_Registro2' alt="Logo" />
+
+
+            <div className="Registro_container2">
+                <h2>Registre-se</h2>
+                <form>
+                    <div>
+                        <input className='Usuario2' type="text" placeholder="Digite seu nome" />
+                        <input className='Usuario2' type="text" placeholder="Digite seu sobrenome" />
+                    </div>
+                    <div>
+                        <input className='Email2' type="email" placeholder="Digite seu email" />
+                    </div>
+                    <div>
+                        <input className='Senha2' type="password" placeholder="Digite sua senha" />
+                    </div>
+
+                    <div>
+                        <input type="checkbox" id="scales" name="scales" />
+                        <label for="scales">Aceito os termos de privacidade</label>
+                    </div>
+                    <Link to='/LoginPrestador'><button className='Btn_Registro2' type="submit">Registre-se</button></Link>
+                </form>
+            </div>
+        </section>
     );
 };
 
