f897a120adf867caf707495bb4a66acf723f6334
Isabella Pacheco <isabellapacheco404@gmail.com>
2023-12-06 10:20:00 -0300

    pagina de registro add detalhes

--- src/componentes/Registro_Login/Registro/Registro.js
+++ src/componentes/Registro_Login/Registro/Registro.js
@@ -13,35 +13,35 @@ function RegistroCliente() {
     return (
         <div className="reg">
             <div className="auth-containerrc">
-                <h2>CADASTRE-SE</h2>
-                <span className="clientere"> Faça parte da nossa plataforma</span>
+                <h2 className=' titlere colorbluet'>CADASTRE-SE</h2>
+                <span className="clientere colorbluet"> Seja cliente em nossa plataforma e encontre o profissional perfeito para o seu reparo</span>
 
                 <form>
 
                     <div className="flex-row">
-                        <div className="input-container">
+                        <div className="input-container colorblue">
                             <FaUser /><input className='Nomep registroclient' type="text" placeholder='nome...' />
                         </div>
 
-                        <div className="input-container">
+                        <div className="input-container colorblue">
                             <FaUser /><input className='Nomep registroclient' type="text" placeholder='Sobrenome...' />
                         </div>
                     </div>
 
-                    <div className="input-container tamnho">
+                    <div className="input-container colorblue tamnho">
                         <MdBadge /><input className='CPF registroclient ' type='text' placeholder='CPF' />
                     </div>
 
-                    <div className="input-container tamnho">
+                    <div className="input-container colorblue tamnho">
                         <MdEmail /><input className='Emailc registroclient ' type="email" placeholder='E-mail' />
                     </div>
 
                     <div className="flex-row">
-                        <div className="input-container">
+                        <div className="input-container colorblue">
                             <RiLockPasswordFill /><input className='Senhac registroclient' type="password" placeholder='Senha' />
                         </div>
 
-                        <div className="input-container">
+                        <div className="input-container colorblue">
                             <RiLockPasswordFill /><input className='Senhac registroclient' type="password" placeholder='Confirme sua senha' />
                         </div>
                     </div>
@@ -49,13 +49,13 @@ function RegistroCliente() {
 
                 </form>
 
-                <div className='termos'>
+                <div className='termos colorbluet'>
                     <input type="checkbox" id="aceitarTermos" name="scales" />
-                    <label htmlFor="aceitarTermos">Aceito os termos de privacidade</label>
+                    <label htmlFor="aceitarTermos colorblue">Aceito os termos de privacidade</label>
                 </div>
 
 
-                <Link to='/LoginCliente'><button className='Btn_Registrop' type="submit">CADASTRE-SE</button></Link>
+                <Link to='/LoginCliente'><button className='Btn_Registrop colorblue' type="submit">CADASTRE-SE</button></Link>
             </div>
         </div>
     );
