43f1cfcb5767ee79abdd8ce90e157657feb79f4c
Paula Martins <lea.paulinha@gmail.com>
2023-12-06 16:59:19 -0300

    arrumei login

--- src/componentes/Registro_Login/Login/Login2.js
+++ src/componentes/Registro_Login/Login/Login2.js
@@ -10,24 +10,24 @@ function LoginPrestador() {
                 <img src={Logo} className='Logo_Login' alt="Logo" />
             </div>
 
-        <div className="flex">
-            <div className="Login_container">
-                <h2>Login</h2>
-                <form>
-                    <div>
-                        <input className='Email' type="email" placeholder="Digite seu email" />
-                    </div>
-                    <div>
-                        <input className='Senha' type="password" placeholder="Digite sua senha" />
-                    </div>
+            <div className="flex">
+                <div className="Login_container">
+                    <h2>Login</h2>
+                    <form>
+                        <div>
+                            <input className='Email' type="email" placeholder="Digite seu email" />
+                        </div>
+                        <div>
+                            <input className='Senha' type="password" placeholder="Digite sua senha" />
+                        </div>
 
-                    <div>
-                        <h3>Esqueci a senha</h3>
-                    </div>
-                    <Link to='/PerfilPrestador'><button className='Btn_Login' type="submit">Login</button></Link>
-                </form>
+                        <div>
+                            <h3>Esqueci a senha</h3>
+                        </div>
+                        <Link to='/PerfilPrestador'><button className='Btn_Login' type="submit">Login</button></Link>
+                    </form>
+                </div>
             </div>
-        </div>
         </section>
     );
 };
